0524742b2deb033bce86e2849983e8c1727f1c35
RaghuManeksha B <baskarmaneksha@gmail.com>
2018-01-01 12:46:11 -0800

    1st version

---
+++ RandomCricketMatch.java
@@ -0,0 +1,149 @@
+
+package randomcricketmatch;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Bat
+{
+    void IndianPlayers()
+    {
+     
+                System.out.println(" "+" "+"Indian Team:");
+
+        ArrayList<String> India_batsman = new ArrayList<String>();
+
+        
+        India_batsman.add("Dhoni");
+        India_batsman.add("Gautham gambhir");
+        India_batsman.add("Virat kholi");
+        India_batsman.add("Virendar sehwag");
+        India_batsman.add("Suresh Raina");
+        India_batsman.add("Yuvraj singh");
+        
+        for(String st : India_batsman)
+        {
+            System.out.println("Batsman:"+" "+st);
+        }
+    }
+    
+    
+    void PakistanPlayers()
+    {
+        
+        
+                        
+        System.out.println(" "+" "+"pakistan Team:");
+
+                        
+        ArrayList<String> Pakistan_batsman = new ArrayList<String>();
+
+        
+        Pakistan_batsman.add("Ahmed shehzad");
+        Pakistan_batsman.add("Imran nazir");
+        Pakistan_batsman.add("Mohammed hafiz");
+        Pakistan_batsman.add("Nazir jamshed");
+        Pakistan_batsman.add("Salman Butt");
+        Pakistan_batsman.add("Umar Akmal");
+        
+        for(String st : Pakistan_batsman)
+        {
+            System.out.println("Batsman:"+" "+st);
+        }
+    }
+    
+    
+}
+
+class Bowl
+{
+    void IndianPlayers()
+    {
+        ArrayList<String> India_bowler = new ArrayList<String>();
+        
+        India_bowler.add("Irfan pathan");
+        India_bowler.add("Yusuf pathan");     
+        India_bowler.add("Harbhajan singh");
+        India_bowler.add("Shree santh");   
+        India_bowler.add("Vinay kumar");
+        
+        
+        for(String st : India_bowler)
+        {
+            System.out.println("Bowler:"+" "+st);
+        }
+        
+         
+        System.out.println("______________________________________");
+
+    }
+    
+    
+    void PakistanPlayers()
+    {
+        ArrayList<String> Pakistan_bowler = new ArrayList<String>();
+        
+        Pakistan_bowler.add("Abdur rauf");
+        Pakistan_bowler.add("Mohammad rasif");     
+        Pakistan_bowler.add("Mohammed sami");
+        Pakistan_bowler.add("Umar gul");   
+        Pakistan_bowler.add("Mohammed irfan");
+        
+        
+        for(String st : Pakistan_bowler)
+        {
+            System.out.println("Bowler"+" "+st);
+        }
+        
+        
+                
+        System.out.println("______________________________________");
+
+    }
+    
+}
+
+
+public class RandomCricketMatch {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome");
+        
+        System.out.println("Team 1:");
+        String team1=sc.next();
+        
+        System.out.println("Team 2:");
+        String team2=sc.next();
+        
+        System.out.println("Overs:");
+        int overs=sc.nextInt();
+        
+        System.out.println("______________________________________");
+
+        
+        System.out.println(team1+" "+"VS"+" "+team2+" "+":"+" "+overs+" "+"Overs");
+        
+        System.out.println("______________________________________");
+        
+        Bat bat=new Bat();
+        Bowl bowl=new Bowl();
+        
+       
+        bat.IndianPlayers();
+        bowl.IndianPlayers();
+        
+        bat.PakistanPlayers();
+        bowl.PakistanPlayers();
+
+        
+        
+        
+
+        
+        
+    }
+    
+}
